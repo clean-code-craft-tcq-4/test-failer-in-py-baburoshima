e43bc40bb63f6f727adec4c0538b34afd64d6b46
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-06-20 12:26:40 +0530

    Update for alerter.py

--- alerter.py
+++ alerter.py
@@ -1,15 +1,32 @@
 alert_failure_count = 0
+THRESHOLD_TEMP = 200
+STUB_CODE_ENABLED = 1
 
 def network_alert_stub(celcius):
     print(f'ALERT: Temperature is {celcius} celcius')
     # Return 200 for ok
     # Return 500 for not-ok
     # stub always succeeds and returns 200
-    return 200
+    return failure_alert(celcius)
+
+def network_alert(celcius):
+    print(f'ALERT: Temperature is {celcius} celcius')
+    return failure_alert(celcius)
+
+def failure_alert(celcius):
+    if(celcius<=THRESHOLD_TEMP) or STUB_CODE_ENABLED == 1:      # stub always succeeds and returns 200
+      returnCode = 200     # Return 200 for ok
+    elif(celcius>THRESHOLD_TEMP):
+      returnCode = 500     # Return 500 for not-ok
+    return returnCode
+
 
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
-    returnCode = network_alert_stub(celcius)
+    if(STUB_CODE_ENABLED):
+        returnCode = network_alert_stub(celcius)
+    else:
+        returnCode = network_alert(celcius)
     if returnCode != 200:
         # non-ok response is not an error! Issues happen in life!
         # let us keep a count of failures to report
@@ -19,7 +36,9 @@ def alert_in_celcius(farenheit):
         alert_failure_count += 0
 
 
-alert_in_celcius(400.5)
 alert_in_celcius(303.6)
+assert(alert_failure_count==0)
+alert_in_celcius(400.5)
+assert(alert_failure_count==1)
 print(f'{alert_failure_count} alerts failed.')
 print('All is well (maybe!)')
