9341fc5d4eb2fde109c90af38536dc39c6edb130
baburoshima <105203651+baburoshima@users.noreply.github.com>
2022-06-24 14:42:48 +0530

    Updates for Pass with Single Responsibility

--- alerter.py
+++ alerter.py
@@ -1,38 +1,34 @@
 from alerterstub import network_alert_stub 
 alert_failure_count = 0
-THRESHOLD_TEMP = 200
+THRESHOLD_TEMP_inCelsius = 200
 
 def network_alert(celcius):
     print(f'ALERT: Temperature is {celcius} celcius')
-    if(celcius<=THRESHOLD_TEMP):    
+    if(celcius<=THRESHOLD_TEMP_inCelsius):    
       returnCode = 200     # Return 200 for ok
-    elif(celcius>THRESHOLD_TEMP):
+    elif(celcius>THRESHOLD_TEMP_inCelsius):
       returnCode = 500     # Return 500 for not-ok
     return returnCode
 
-
-def alert_in_celcius(farenheit, testenv):
-    celcius = (farenheit - 32) * 5 / 9
-    if testenv=='stub':
-        returnCode = network_alert_stub(celcius)
-    else:
-        returnCode = network_alert(celcius)
+def alert_in_celcius(farenheit, network_alert_func): #improved by using function pointer
+    celcius = (farenheit- 32) * 5 / 9
+    returnCode = network_alert_func(celcius)
     if returnCode != 200:
         # non-ok response is not an error! Issues happen in life!
         # let us keep a count of failures to report
         # However, this code doesn't count failures!
         # Add a test below to catch this bug. Alter the stub above, if needed.
         global alert_failure_count
-        alert_failure_count += 0
+        alert_failure_count += 1
 
 
-alert_in_celcius(303.6, 'stub')
+alert_in_celcius(303.6, network_alert)
 assert(alert_failure_count==0)
-alert_in_celcius(303.6, 'prod')
+alert_in_celcius(303.6, network_alert_stub)
 assert(alert_failure_count==0)
-alert_in_celcius(400.5 ,'stub')
-assert(alert_failure_count==1)
-alert_in_celcius(400.5 , 'prod')
-assert(alert_failure_count==1)
+alert_in_celcius(400.5 , network_alert)
+assert(alert_failure_count!=0)
+alert_in_celcius(400.5 , network_alert_stub)
+assert(alert_failure_count!=0)
 print(f'{alert_failure_count} alerts failed.')
-print('All is well (maybe!)')
+print('All is well')
--- misaligned.py
+++ misaligned.py
@@ -10,26 +10,29 @@ def print_color_map():
             pair_number = get_pairnumber(i,j)
             [first_sep_idx,second_sep_idx] = get_separator_index(format_string(pair_number, major, minor))
             print(format_string(pair_number, major, minor))
-            color_map.append([first_sep_idx,second_sep_idx]) 
-    return pair_number, len(major_colors) * len(minor_colors),color_map
+            color_map.append((pair_number, [first_sep_idx,second_sep_idx]))
+    return len(major_colors) * len(minor_colors),color_map
 
 def format_string(pair_number, major_color, minor_color):
-  return f'{pair_number} | {major_color} | {minor_color}'
+  return f'{pair_number:3} | {major_color:6} | {minor_color}' #added proper space to align separator
 
 def get_separator_index(formatted_string):
     [first_sep_idx,second_sep_idx] = [index for index, separator in enumerate(formatted_string) if separator == '|']
     return [first_sep_idx,second_sep_idx]
 
 def get_pairnumber(major_color, minor_color):
-    return major_color * len(major_colors) + minor_color 
+    return major_color * len(major_colors) + minor_color + 1
 
-#Test function to check formating by separator index  
+#Test function to check 
+# 1.formating by separator index 
+# 2. pair number within range (1,25)
 def test_color_map(color_map):
+    assert(color_map[0][0]==1) # check if the first pair number is 1
     for i in range(pair_number_start,pair_number_end):
         assert([color_map[i][1][0],color_map[i][1][1]]==[color_map[pair_number_end-i][1][0],color_map[pair_number_end-i][1][1]])
+        assert(color_map[i][0] == i+1)
 
-pair_number,result,color_map = print_color_map()
+result,color_map = print_color_map()
 assert(result == 25)
 test_color_map(color_map)
-assert(pair_number==pair_number_end) # test to check last pair number as 25
-print("All is well (maybe!)\n")
+print("All is well :)\n")
